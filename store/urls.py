ef7578d835556ed0b632376617b2188ed4e093eb
Its-ech <its.ech.t9@gmail.com>
2025-12-14 07:48:06 -0500

    Add product detail view and template

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("products/", views.product_list, name="product_list"),
+    path("products/<slug:slug>/", views.product_detail, name="product_detail"),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import render, get_object_or_404
+
 
 def home(request):
     context = {
@@ -7,3 +10,11 @@ def home(request):
         "tagline": "learning Django step by step",
     }
     return render(request, "store/home.html", context)
+
+def product_list(request):
+    products = Product.objects.filter(is_active=True)
+    return render(request, "store/product_list.html", {"products": products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, "store/product_detail.html", {"product": product})

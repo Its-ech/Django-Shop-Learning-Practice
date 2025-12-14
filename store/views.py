970ee4af18e761f05d2cf166270380421281587f
Its-ech <its.ech.t9@gmail.com>
2025-12-14 02:35:31 -0500

    Add base template and static styling

--- store/views.py
+++ store/views.py
@@ -4,5 +4,6 @@ def home(request):
     context = {
         "title": "Django Shop",
         "welcome_message": "Welcome to my Django shop from template!",
+        "tagline": "learning Django step by step",
     }
-    return render(request, "home.html", context)
+    return render(request, "store/home.html", context)
